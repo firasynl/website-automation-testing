4ab9d55a6b445250934f8341ed4003074a01f703
firasyanl <firathifah@gmail.com>
2024-11-06 14:54:23 +0700

    cypres installation

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,29 @@
+describe('login', () => {
+  it('with valid credential', () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.xpath('//a[@id="login2"]').click()
+    cy.xpath('//input[@id="loginusername"]').type('defg')
+    cy.wait(2000)
+    cy.xpath('//input[@id="loginpassword"]').type('abcd123')
+    cy.wait(2000)
+    cy.xpath('//button[text()="Log in"]').click()
+    cy.wait(5000)
+    cy.xpath('//a[@id="logout2"]').should('exist')
+    cy.xpath('//a[@id="nameofuser"]').should('exist')
+  }),
+  it('with invalid credential', () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.xpath('//a[@id="login2"]').click()
+    cy.xpath('//input[@id="loginusername"]').type('defg')
+    cy.wait(2000)
+    cy.xpath('//input[@id="loginpassword"]').type('abcd')
+    cy.wait(2000)
+    cy.xpath('//button[text()="Log in"]').click()
+    cy.wait(2000)
+  })
+  it('empty field', () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.xpath('//a[@id="login2"]').click()
+    cy.xpath('//button[text()="Log in"]').click()
+  })
+})
---
+++ cypress/e2e/sign-up.cy.js
@@ -0,0 +1,15 @@
+describe('sign-up', () => {
+  it('with valid credential', () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.xpath('//a[@id="signin2"]').click()
+    cy.xpath('//input[@id="sign-username"]').type('abcd@test.com')
+    cy.wait(2000)
+    cy.xpath('//input[@id="sign-password"]').type('abc123')
+    cy.xpath('//button[text()="Sign up"]').click()
+  })
+  it('empty field', () => {
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.xpath('//a[@id="signin2"]').click()
+    cy.xpath('//button[text()="Sign up"]').click()
+  })
+})
